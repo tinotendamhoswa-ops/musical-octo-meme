6de3245a4f92914811dc195fe22d5c746bf6f3e1
tinotendamhoswa-ops <tinotendamhoswa@gmail.com>
2026-03-20 19:16:46 +0200

    Add InvoiceNudge component to app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InvoiceNudge = () => {
+    return (
+        <div>
+            <h1>Invoice Nudge</h1>
+            <p>Send reminders to clients for unpaid invoices.</p>
+        </div>
+    );
+};
+
+export default InvoiceNudge;
